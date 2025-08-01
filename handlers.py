0bdd519a816a08884c27029b6571078cea6f4644
sobir0630 <mamasoliyevs538@gmail.com>
2025-08-01 19:26:53 +1030

    bot yaratildi

--- handlers.py
+++ handlers.py
@@ -2,20 +2,60 @@ from telegram import Update
 from telegram.ext import CallbackContext
 
 
+
 def start(update: Update, context: CallbackContext):
     user = update.message.from_user
 
     update.message.reply_text(
         f'Assalomu alaykum! Botimizga xush kelibsiz {user.full_name}.\n\n'
-        'bu bot testing uchun'
+        'bu bot testing uchun /help komandasini bosing.\n\n'
     )
 
 
+# /help komandasini ishlovchi funksiya
 def help(update: Update, context: CallbackContext):
     update.message.reply_text(
-        'Qanday Yordam Kerak'
+        'Qanday Yordam Kerak\n\n'
+        '/start - Botni ishga tushirish\n'
+        '/help - Yordam\n'
     )
 
-
+# reply text handlers
 def echo(update: Update, context: CallbackContext):
     update.message.reply_text(update.message.text)
+    
+#  reply photo handlers   
+def echo_photo(update: Update, context: CallbackContext):
+    update.message.reply_photo(update.message.photo[-1].file_id, caption=update.message.caption)
+
+#  reply video handlers
+def echo_video(update: Update, context: CallbackContext):
+    update.message.reply_video(update.message.video.file_id, caption=update.message.caption)
+
+#  reply animation handlers
+def echo_animation(update: Update, context: CallbackContext):
+    animation = update.message.animation
+    update.message.reply_animation(animation=animation.file_id, caption=update.message.caption)
+
+# echo location
+def echo_location(update: Update, context: CallbackContext):
+    update.message.reply_location(
+        latitude=update.message.location.latitude,
+        longitude=update.message.location.longitude
+    )
+
+# reply voice handlers
+def echo_voice(update: Update, context: CallbackContext):
+    update.message.reply_voice(update.message.voice.file_id, caption=update.message.caption)
+
+# reply video_note handlers
+def echo_video_note(update: Update, context: CallbackContext):
+    update.message.reply_video_note(update.message.video_note.file_id)
+
+# reply video_note handlers
+def echo_contact(update: Update, context: CallbackContext):
+    update.message.reply_contact(
+        phone_number=update.message.contact.phone_number,
+        first_name=update.message.contact.first_name,
+        last_name=update.message.contact.last_name
+    )
--- main.py
+++ main.py
@@ -2,7 +2,18 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from config import TOKEN
 
-from handlers import start, help, echo
+from handlers import(
+    start, 
+    help,
+    echo, 
+    echo_photo, 
+    echo_video, 
+    echo_location,
+    echo_animation,
+    echo_voice,
+    echo_video_note,
+    echo_contact
+)
 
 
 updater = Updater(TOKEN)
@@ -12,8 +23,17 @@ dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('help', help))
 
+
 # message handler
 dispatcher.add_handler(MessageHandler(Filters.text, echo))
+dispatcher.add_handler(MessageHandler(Filters.photo, echo_photo))
+dispatcher.add_handler(MessageHandler(Filters.video, echo_video))
+dispatcher.add_handler(MessageHandler(Filters.location, echo_location))
+dispatcher.add_handler(MessageHandler(Filters.animation, echo_animation))
+dispatcher.add_handler(MessageHandler(Filters.voice, echo_voice))
+dispatcher.add_handler(MessageHandler(Filters.video_note, echo_video_note))
+dispatcher.add_handler(MessageHandler(Filters.contact, echo_contact))
 
+print("Bot is running...")  # Optional: Print a message to indicate the bot is running
 updater.start_polling()
 updater.idle()
